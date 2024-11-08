9de4e1bda503b32ce713b912707437d654b95338
Juan Ramon Rios <juan.rios.ext@orangebank.es>
2024-11-08 14:27:46 +0100

    Add sqlDialect and databaseStructure in SqlDatabaseContentRetriever

--- src/main/java/com/izertis/ai/langchain4j/rag/sql/config/FilmsAIConfig.java
+++ src/main/java/com/izertis/ai/langchain4j/rag/sql/config/FilmsAIConfig.java
@@ -42,6 +42,17 @@ public class FilmsAIConfig {
         return SqlDatabaseContentRetriever.builder()
                 .dataSource(dataSource)
                 .chatLanguageModel(chatLanguageModel)
+                .sqlDialect("H2")
+                .databaseStructure(
+                        """
+                        CREATE TABLE movie ( id VARCHAR(10) NOT NULL, title VARCHAR(200) DEFAULT NULL, release_year INT DEFAULT NULL, date_published DATE DEFAULT null, duration INT, country VARCHAR(250), worlwide_gross_income VARCHAR(30), languages VARCHAR(200), production_company VARCHAR(200), PRIMARY KEY (id) );
+                        CREATE TABLE genre ( movie_id VARCHAR(10), genre VARCHAR(20), PRIMARY KEY (movie_id, genre) );
+                        CREATE TABLE director_mapping ( movie_id VARCHAR(10), name_id VARCHAR(10), PRIMARY KEY (movie_id, name_id) );
+                        CREATE TABLE role_mapping ( movie_id VARCHAR(10) NOT NULL, name_id VARCHAR(10) NOT NULL, category VARCHAR(10), PRIMARY KEY (movie_id, name_id) );
+                        CREATE TABLE names ( id varchar(10) NOT NULL, name varchar(100) DEFAULT NULL, height int DEFAULT NULL, date_of_birth date DEFAULT null, known_for_movies varchar(100), PRIMARY KEY (id) );
+                        CREATE TABLE ratings ( movie_id VARCHAR(10) NOT NULL, avg_rating DECIMAL(3,1), total_votes INT, median_rating INT, PRIMARY KEY (movie_id) );
+                        """
+                )
                 .build();
     }
 
